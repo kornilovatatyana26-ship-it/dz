c7e52934ab654ceb9889fa4c3c703242419fc31a
Tatyana <kornilovatatyana26-ship-it@users.noreply.github.com>
2026-05-20 16:05:48 +0300

    make greeting website

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const statusText = document.querySelector("#statusText");
-const statusButton = document.querySelector("#statusButton");
-const progressCard = document.querySelector(".progress-card");
+const wishText = document.querySelector("#wishText");
+const wishButton = document.querySelector("#wishButton");
 
-statusButton.addEventListener("click", () => {
-  statusText.textContent = "Готово: проект можно отправлять на GitHub.";
-  statusButton.textContent = "Готово";
-  progressCard.classList.add("is-complete");
+wishButton.addEventListener("click", () => {
+  wishText.textContent = "Улыбнись: у тебя уже получается работать с Git!";
 });
